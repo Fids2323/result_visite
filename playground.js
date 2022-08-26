554dad76aba6882f99edef8e6f26a92341162ffa
Fids2323 <workfids23@gmail.com>
2022-08-26 23:29:52 +0300

    add js

---
+++ playground.js
@@ -0,0 +1,50 @@
+// const controller = new ScrollMagic.Controller()
+
+// const scene = new ScrollMagic.Scene({
+// 	triggerElement: '.git',
+// 	triggerHook: 0.5,
+// }).addIndicators()
+// 	.setClassToggle('.git', "active")
+// 	.addTo(controller)
+
+
+
+
+
+//Get h2 from section javascript
+//const title = document.querySelector('.javascript h2')
+
+//scroll/location
+// window.addEventListener('scroll', () => {
+// 	const size = title.getBoundingClientRect()
+// 	if (size.top < window.innerHeight / 3) {
+// 		title.style.background = 'red'
+// 	}
+// })
+
+
+
+
+// //get img from section git
+// const img = document.querySelector('.git img')
+
+// //create object with param threshold(0.5- in to hight element)
+// const options = {
+// 	threshold: 0.5,
+// }
+// //class create observer
+// const observer = new IntersectionObserver(animate, options)
+
+// //entries-array
+// function animate(entries) {
+// 	entries.forEach((entry) => {
+// 		if (entry.isIntersecting) {
+// 			img.style.transition = 'transform .5s ease-in'
+// 			img.style.transform = 'scale(2)'
+// 		}
+// 	});
+// }
+
+// observer.observe(img)
+
+
